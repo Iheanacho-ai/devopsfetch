6381200deb04fc948975849f9638c14d4f4bea0c
Amarachi Iheanacho <amarachiiheanacho@Amarachis-MacBook-Pro.local>
2024-07-24 21:19:47 +0100

    fixed bugs

--- devopsfetch.sh
+++ devopsfetch.sh
@@ -15,14 +15,27 @@ display_help() {
 
 LOG_FILE="devopsfetch.log"
 
+# Generate logs and passwords 
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1 - $2" >> $LOG_FILE
+}
+
 # display all active ports
 display_ports() {
     if [ -z "$1" ]; then 
         echo "Active ports and services"
-        ss -tuln | column -t
+        if ss -tuln | column -t; then
+            log_message "INFO" "Listed all active ports and services"
+        else
+            log_message "ERROR" "Error listing out the active ports and services"
+        fi
     else
         echo "Display information on port: $1"
-        ss -lntu | grep ":$1" | column -t
+        if ss -lntu | grep ":$1" | column -t; then
+            log_message "INFO" "Listed information on port $1"
+        else
+            log_message "ERROR" "Error listing out information on port $1"
+        fi
     fi
 }
 
@@ -30,12 +43,20 @@ display_ports() {
 display_docker() {
     if [ -z "$1" ]; then
         echo "Docker images:"
-        docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.ID}}"
-        echo "Docker containers:"
-        docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
+        if docker images --format "table {{.Repository}}\t{{.Tag}}\t{{.ID}}"; then
+            echo "Docker containers:"
+            docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
+            log_message "INFO" "Listed all Docker images and containers"
+        else 
+            log_message "ERROR" "Error listing out all the docker images and containers"
+        fi
     else
         echo "Information on $1"
-        docker inspect "$1"
+        if docker inspect "$1"; then
+            log_message "INFO" "Provided detailed information for $1"
+        else
+            log_message "ERROR" "Error listing out information for $1"
+        fi
     fi
 }
 
@@ -43,9 +64,17 @@ display_docker() {
 display_nginx() {
     if [ -z "$1" ]; then 
         echo "Nginx domains and ports:"
-        grep -E 'server_name|listen' /etc/nginx/sites-enabled/* | paste - - | awk '{print $1, $3}' | column -t 
+        if grep -E 'server_name|listen' /etc/nginx/sites-enabled/* | paste - - | awk '{print $1, $3}' | column -t; then
+            log_message "INFO" "Listed all Nginx domains and ports"
+        else
+            log_message "ERROR" "Error listing out all the Nginx domains and ports"
+        fi
     else
-        grep -A10 "server_name $nginx" /etc/nginx/sites-enabled/* | column -t
+        if grep -A10 "server_name $nginx" /etc/nginx/sites-enabled/* | column -t; then
+            log_message "INFO" "Provided detailed information for server $1"
+        else
+            log_message "ERROR" "Error providing the detailed information for server $1"
+        fi
     fi
 }
 
@@ -53,10 +82,18 @@ display_nginx() {
 display_users() {
     if [ -z "$1" ]; then
         echo "Users and last login times:"
-        last -a | column -t
+        if last -a | column -t; then
+            log_message "INFO" "Listed all users and their last logged in times"
+        else
+            log_message "ERROR" "Error listing all the users"
+        fi
     else
         echo "Detailed information for user: $1"
-        last -a | grep "$1" | column -t
+        if last -a | grep "$1" | column -t; then
+            log_message "INFO" "Provided detailed information for user $1"
+        else
+            log_message "ERROR" "Error providing information for user $1"
+        fi
     fi
 }
 
@@ -64,10 +101,18 @@ display_users() {
 display_time_range() {
    if [ -z "$2" ]; then # Check if only one argument was provided
         echo "Activities on $1:"
-        journalctl --since "$1" --until "$1 23:59:59" | tail -n 50 
+        if journalctl --since "$1" --until "$1 23:59:59" | tail -n 50; then
+            log_message "INFO" "Listed all the activities on day $1"
+        else 
+            log_message "ERROR" "Error listing out all the activities on day $1"
+        fi
     else
         echo "Activities between $1 and $2:"
-        journalctl --since "$1" --until "$2" | tail -n 50
+        if journalctl --since "$1" --until "$2" | tail -n 50; then
+            log_message "INFO" "Listed all activities that happened between $1 and $2"
+        else 
+            log_message "ERROR" "Error listing out activities that happened between $1 and $2"
+        fi
     fi
 
 }
@@ -77,23 +122,23 @@ display_time_range() {
 while [ "$#" -gt 0 ]; do
     case $1 in 
         -p|--port) 
-            display_ports "$2" | tee -a "$LOG_FILE"
+            display_ports "$2" 
             shift
             ;;
         -d|--docker) 
-            display_docker "$2" | tee -a "$LOG_FILE"
+            display_docker "$2"
             shift
             ;;
         -n|--nginx) 
-            display_nginx "$2" | tee -a "$LOG_FILE"
+            display_nginx "$2"
             shift
             ;;
         -u|--users) 
-            display_users "$2" | tee -a "$LOG_FILE"
+            display_users "$2" 
             shift
             ;;
         -t|--time) 
-            display_time_range "$2" "$3" | tee -a "$LOG_FILE"
+            display_time_range "$2" "$3"
             shift
             ;;
         -h|--help) 
